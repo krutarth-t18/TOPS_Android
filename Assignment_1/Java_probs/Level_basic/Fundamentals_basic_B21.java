f050756489bdcfa7fd6b0dd5438682daa49a7f92
Krutarth Trivedi <68107108+krutarth-t18@users.noreply.github.com>
2021-07-23 12:33:26 +0530

    Add files via upload

---
+++ Assignment_1/Java_probs/Level_basic/Fundamentals_basic_B21.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Fundamentals_basic_B21 {
+    public static void main(String[] args) {
+        for(int i=0;i<9;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
+/*
+1
+12
+123
+1234
+12345
+ */
