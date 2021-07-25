49f01812a420e5587c811fd9815e6de4809267eb
Krutarth Trivedi <68107108+krutarth-t18@users.noreply.github.com>
2021-07-25 15:54:34 +0530

    Add files via upload

---
+++ Assignment_1/Java_probs/Level_basic/String_basic_B19.java
@@ -0,0 +1,11 @@
+package com.company;
+import java.util.Scanner;
+
+public class String_basic_B19 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str1 = "TOPS";
+        String str2 = "Tops";
+        System.out.println("Comparing "+str1+" and "+str2+": "+(str1==str2));
+    }
+}
